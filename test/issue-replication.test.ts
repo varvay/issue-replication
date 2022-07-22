0e541e18a3e7fa2bec9503b3586cb7155e59e925
varvay <varidvaya@outlook.com>
2022-07-22 20:49:23 +0700

    Initial commit

---
+++ test/issue-replication.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IssueReplication from '../lib/issue-replication-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/issue-replication-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IssueReplication.IssueReplicationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
